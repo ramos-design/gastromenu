fd53a03fba599e41129a7b95c3e37e01e1a020cc
ramos-design <artmovski@gmail.comz>
2026-01-29 21:44:57 +0100

    nova aktualizace čtvrtek

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true, // Ignorujeme chyby typechecku při buildu
   },
   eslint: {
     ignoreDuringBuilds: true,

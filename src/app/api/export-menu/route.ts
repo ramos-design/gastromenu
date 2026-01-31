50f86a7679c540bb276ea45c77628bc24128623a
ramos-design <artmovski@gmail.comz>
2026-01-31 12:11:59 +0100

    akt1

--- src/app/api/export-menu/route.ts
+++ src/app/api/export-menu/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams;
-    const n8nWebhookUrl = 'https://n8n.srv1004354.hstgr.cloud/webhook/d27670eb-ad4a-42ed-9b6f-acd4b00f78e6';
+    const n8nWebhookUrl = 'https://n8n.srv1004354.hstgr.cloud/webhook-test/d27670eb-ad4a-42ed-9b6f-acd4b00f78e6';
 
     try {
         const finalUrl = `${n8nWebhookUrl}?${searchParams.toString()}`;
